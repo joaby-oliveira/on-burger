fbe635783959cce3981089fba3ee265d98e41b98
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-08-27 09:26:54 -0300

    feat: add navbar to all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import styles from '../../styles/landing.module.scss'
-import Landing from './landing/Landing'
+import Navbar from './Components/Navbar'
+import Landing from './Landing'
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>On Burger</title>
         <meta name="description" content="On Burger hamburgueria lanches artesanais" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar />
       <main className={styles.main}>
         <Landing />
       </main>
