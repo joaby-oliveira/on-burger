6f5fc81ba18d4016d20188bd8b7059c2e8d8a9ad
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-08-30 16:44:29 -0300

    feat: create top bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
-import styles from '../../styles/landing.module.scss'
 import Navbar from './Components/Navbar'
+import Topbar from './Components/Topbar'
 import Landing from './Landing'
 export default function Home() {
   return (
     <div>
+      <Topbar />
       <Head>
         <title>On Burger</title>
         <meta name="description" content="On Burger hamburgueria lanches artesanais" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className={styles.main}>
+      <main>
         <Landing />
       </main>
     </div>
